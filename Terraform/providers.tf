ecb9d8e4eb6f8b4d988b685c5eef258088dcffe1
Antony John Paul <john.paul@omnicell.com>
2021-12-08 22:43:53 -0500

    cleanup

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIAS6ONEI2BV6HK76PL"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   region     = "us-east-2"
 }
